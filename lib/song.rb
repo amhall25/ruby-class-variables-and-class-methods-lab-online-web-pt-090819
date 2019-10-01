8eab9e442a9230aef57fe4ff1c1653809816d188
alycia hall <amhall25@gmail.com>
2019-10-01 16:40:51 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count=0
+  @@artists=[]
+  @@genres=[]
+  def initialize(name, artist, genre)
+    @@count+=1
+    @name=name
+    @artist=artist
+    @@artists<<@artist
+    @genre=genre
+    @@genres<<@genre
+  end
+  
+  def count
+    @@count
+  end
+end
+  
